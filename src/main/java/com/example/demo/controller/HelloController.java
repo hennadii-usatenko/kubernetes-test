4df3b88fdf6bbdcfe3463754c1bd6b6382cd4446
GennadiyUsatenko <usatenko.gena@gmail.com>
2021-11-30 21:34:17 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Void> hello() {
+        log.info("Got hello request");
+        return ResponseEntity.noContent().build();
+    }
+}
